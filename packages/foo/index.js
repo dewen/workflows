7ebaf28400518fde56af6537a17beb18f92787b1
Dewen Li <dli94@its.jnj.com>
2020-12-06 20:29:39 -0500

    foo updates

--- packages/foo/index.js
+++ packages/foo/index.js
@@ -1,5 +1,5 @@
 module.exports = () => {
-  console.log('dewen: foo v0.0.4 a1')
+  console.log('dewen: foo v0.0.4 rel.v0.0.13')
   console.log('dewen: this just a silly demo about using lerna along with chan and a publish-from-travis setup')
   console.log('dewen: 🔗 see more here: https://github.com/geut/lerna-travis-demo')
 }
